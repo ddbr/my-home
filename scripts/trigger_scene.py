30afe07d9e4e8b091ad645ff7e064a351c3f30fc
ddbr <daniel.braeker@gmail.com>
2025-04-27 10:46:30 +0200

    update

--- scripts/trigger_scene.py
+++
@@ -1,9 +0,0 @@
-# scripts/trigger_button.py
-from src.registry.manager import load_registry
-from src.homekit.driver import HomeKitButtonServer
-
-registry = load_registry()
-buttons = registry.get("homekit_buttons", {})
-
-server = HomeKitButtonServer(buttons_registry=buttons)
-server.trigger_button("scene_1", press_type=0)
